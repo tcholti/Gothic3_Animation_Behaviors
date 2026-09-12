97da1d494e60d6ea7e4bc13a9a14ac92f9739e57
tcholti <tcholti@gmail.com>
2026-09-12 18:44:49 +0200

    Add raw55 Quick callback observation token

--- prototypes/Script_FrameCollisionTest/PhysicalFistProbe.h
+++ prototypes/Script_FrameCollisionTest/PhysicalFistProbe.h
@@ -2,10 +2,34 @@
 
 #include "FrameCollisionMarkers.h"
 
+#include <cstdint>
+#include <string>
+
 namespace FrameCollision::PhysicalFistProbe
 {
+struct QuickCallbackObservation
+{
+    bool active;
+    eCEntity *actorInstance;
+    eCEntity *rightSourceInstance;
+    std::uint64_t c1Generation;
+    GEInt actionBefore;
+    GEInt phaseBefore;
+    GEFloat stateTimeBefore;
+    GEInt statePositionBefore;
+    GEInt rightGroupBefore;
+    std::string motionBefore;
+};
+
 bool ShouldSuppressNativeCallback(
     Entity &actor, AttackFamily family,
     FrameCollisionMarkers::AttackCallbackOwnershipResult const &ownership,
     gCScriptProcessingUnit *spu);
+
+QuickCallbackObservation BeginQuickCallbackObservation(
+    Entity &actor, gCScriptProcessingUnit *spu);
+
+void EndQuickCallbackObservation(
+    Entity &actor, QuickCallbackObservation const &observation,
+    GEBool nativeResult);
 }
